4c5aeeb9c2c499961351e9549e8d8418a8ac1815
Cody Hill <codyjhill99@gmail.com>
2016-06-14 13:15:24 -0400

    Finished Restaurant Bill

--- RestaurantBill/RestaurantBill.java
+++ RestaurantBill/RestaurantBill.java
@@ -1,7 +1,26 @@
+import java.util.Scanner; // imports the scanner
 /**
  * Computes the tax of a bill
  */
 public class RestaurantBill
 {
-    
+    public static void main(String[] args)
+    {
+        //tax is 7.5% tip is 18% after tax
+        //creates the variables for the program
+        double charge,total,tip, tax;
+        
+        //creates the scanner
+        Scanner keyboard = new Scanner(System.in);
+        
+        //asks the user for charge amount
+        System.out.println("What was your charge amount?");
+        charge = keyboard.nextDouble();
+        
+        //does the calulations for the final output
+        tax = charge * .075;
+        tip = (charge + tax) * .18;
+        total = charge + tax + tip;
+        System.out.printf("Meal charge : %.2f\nTax: %.2f\nTip: %.2f\nYour total is: %.2f.",charge ,tax ,tip , total);
+    }
 }
