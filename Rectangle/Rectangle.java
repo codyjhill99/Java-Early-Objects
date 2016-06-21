dce4ba0b1e1f9ee0172394c36f7546bcc79dc868
Cody Hill <codyjhill99@gmail.com>
2016-06-21 15:57:21 -0400

    Add setWidth

--- Rectangle/Rectangle.java
+++ Rectangle/Rectangle.java
@@ -6,8 +6,15 @@ public class Rectangle
     private double length;
     private double width;
     
+    //the setLength method accepts and argument that is stored in the length field;
     public void setLength(double len)
     {
         length = len;
+    }
+    
+    //the setWidth method accepts an argument that is stored in the width field.
+    public void setWidth(double w)
+    {
+        width = w;
     }
 }
