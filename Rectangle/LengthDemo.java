e9df43290ebe9ae6ddf1a4366c7d10d43f6705c3
Cody Hill <codyjhill99@gmail.com>
2016-06-21 15:49:44 -0400

    Add LengthDemo

---
+++ Rectangle/LengthDemo.java
@@ -0,0 +1,15 @@
+/**
+ * This program demonstrates the rectangle class's setLength method.
+ */
+public class LengthDemo
+{
+    public static void main(String[] args)
+    {
+        Rectangle box = new Rectangle();
+        
+        System.out.println("Sendng the value 10.0 to the setLength method.");
+        box.setLength(10.0);
+        System.out.println("Done.");
+        
+    }
+}
