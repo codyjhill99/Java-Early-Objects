8b73a290f49aa3c7327fd6cbd5a43ed126a17233
Cody Hill <codyjhill99@gmail.com>
2016-06-21 15:39:57 -0400

    Add Rectangle

---
+++ Rectangle/Rectangle.java
@@ -0,0 +1,13 @@
+/**
+ * This is a class for making rectangles. Practice
+ */
+public class Rectangle
+{
+    private double length;
+    private double width;
+    
+    public void setLength(double len)
+    {
+        length = len;
+    }
+}
